d2d4fd650130775bc1e9696d86bc5486c93a52da
Jenney <fanjunni2006@163.com>
2019-11-08 14:52:07 +0800

    HTML标签转义

--- src/component/Comment.js
+++ src/component/Comment.js
@@ -42,13 +42,22 @@ class Comment extends Component {
     _isShow(n,str){
         return n > 0 ? n + str + '前' : '';
     }
+    _getProcessedContent (content) {
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
 
     render() {
         return (
             <div className='comment'>
                 <div className='comment-user'>
                     <span>{this.props.comment.username}</span> :
-                    <p>{this.props.comment.content}</p>
+                    <p dangerouslySetInnerHTML={{__html:this._getProcessedContent(this.props.comment.content)}} />
                     <span className='comment-createdtime'>{this.state.timeString}</span>
                     <span
                         onClick={(e)=>this.handleDeleteComment(e)}
